fa4d6bfeaffa034369221c2d7a34afff530fa886
dairujia <dairujia@qianxin.com>
2021-01-11 15:12:06 +0800

    增加侧边栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,5 +10,50 @@ module.exports = {
         href: '/banana_leaf.png'
       }
     ]
-  ]
+  ],
+  themeConfig: { // 默认主题的相关配置，比如顶部顶部导航栏，侧边栏等等
+    logo: '/banana_leaf.png', // 网页顶端导航栏左上角的图标，路径跟上面一样，第一个/会遍历public文件夹
+    // 顶部导航栏
+    nav: [
+      { text: '首页', link: '/'}, // 直接跳转，/为不添加路由，跳转到首页
+      // {
+      //   text: '分类', // 默认显示
+      //   ariaLabel: '分类', // 用于识别的label，讲真我还没明白这个属性是干嘛的
+      //   items: [
+      //     { text: '框架', link: '/pages/framework/angular/angular.md' },
+      //     { text: '算法', link: '/pages/leetcode/leetcode.md' }
+      //   ]
+      // },
+      // { text: '琐碎', link: '/pages/other/readme.md' },
+      { text: 'Github', link: 'https://github.com/dairj', target: '_blank' }
+    ],
+    sidebar: [
+      ['/pages/about/', '介绍'],
+      {
+        title: '框架',
+        path: '/pages/framework/',
+        collapsable: false,
+        children: [
+          {
+            title: 'Angular',
+            path: '/pages/framework/angular',
+            collapsable: false,
+            children: [
+              '/pages/framework/angular/angular'
+            ]
+          },
+          {
+            title: 'React',
+            path: '/pages/framework/react/',
+            collapsable: false,
+          },
+          {
+            title: 'Vue',
+            path: '/pages/framework/vue/',
+            collapsable: false,
+          }
+        ]
+      }
+    ]
+  }
 }
